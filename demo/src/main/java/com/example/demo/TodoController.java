a77f8bb22c89fbc8fe0d2383d5723f2f67e4dabf
Jhon salazar <jhonzallazar@gmail.com>
2021-11-29 20:52:49 -0500

    Se agregaron las partes del front-end  asi como la conexion con el back-end, permitiendo la implementacion de las tareas

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -1,14 +1,10 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
     
     @Autowired
@@ -21,15 +17,15 @@ public class TodoController {
     
     @PostMapping(value = "api/todo")
     public Todo save(@RequestBody Todo todo){
+        return service.save(todo);
+    }
+    
+    @PutMapping(value = "api/todo")
+    public Todo update(@RequestBody Todo todo){
         if(todo.getId() != null){
             return service.save(todo);
         }
         throw new RuntimeException("No existe el id para actualizar");
-    }
-    
-    @PostMapping(value = "api/todo")
-    public Todo update(@RequestBody Todo todo){
-        return service.save(todo);
     }
     
     @DeleteMapping(value = "api/{id}/todo")
